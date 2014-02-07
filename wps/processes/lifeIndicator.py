708c4b66ee5bffa9a5a14e87749145100038fd66
Peter Kutschera <peter.kutschera@ait.ac.at>
2014-02-07 08:09:18 +0100

    added license headers

--- wps/processes/lifeIndicator.py
+++ wps/processes/lifeIndicator.py
@@ -10,8 +10,26 @@ The server gets an world state id and calculates an indicator
 
 Needs an recent requests library:
 pip install requests --upgrade
+"""
 
 """
+    Copyright (C) 2014  AIT / Austrian Institute of Technology
+    http://www.ait.ac.at
+ 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as
+    published by the Free Software Foundation, either version 2 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+ 
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/gpl-2.0.html
+"""
+
 
 from pywps.Process import WPSProcess                                
 from sys import stderr
