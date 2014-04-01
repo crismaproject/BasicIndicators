38f83117503d917941afe511c3b77c34f9b06f81
Peter Kutschera <peter.kutschera@ait.ac.at>
2014-04-01 14:53:39 +0200

    Updated indicators test page to get list of indicators from GetCapabilities

--- web/libs/wpsControllers.js
+++ web/libs/wpsControllers.js
@@ -1,11 +1,49 @@
 var wpsApp = angular.module ("wpsApp", []);
+var x2js = new X2JS();     
 
 
 wpsApp.controller ('wpsCtrl', function ($scope, $http) {
-    $scope.wpsEndpoint = "https://pinguin2.ait.ac.at/~peter/indicators/pywps.cgi";  # test system
-    # $scope.wpsEndpoint = "https://crisma.ait.ac.at/indicators/pywps.cgi";           # production system
-    $scope.indicators = ["deathsIndicator", "seriouslyDeterioratedIndicator", "improvedIndicator", "lifeIndicator"];
-    $scope.indicator = $scope.indicators[0];
+    // test system:
+    $scope.wpsEndpoint = "https://pinguin2.ait.ac.at/~peter/indicators/pywps.cgi";
+    // production system
+    // $scope.wpsEndpoint = "https://crisma.ait.ac.at/indicators/pywps.cgi";
 
+    // list of indicators from WPS capabilities - to be updated from WPS Capabilities
+    $scope.indicators = [];  // ["deathsIndicator", "seriouslyDeterioratedIndicator", "improvedIndicator", "lifeIndicator"];
+    $scope.indicator = null; // $scope.indicators[0];
+
+    // example ICMM worldstate URL
     $scope.worldstateUrl = "http://crisma.cismet.de/pilotC/icmm_api/CRISMA.worldstates/2";
+
+    // ask WPS for indicators (Processes in GetCapabilities)
+    $scope.loadIndicators = function() {
+	$http({
+	    method: "GET",
+	    url: $scope.wpsEndpoint + "?service=WPS&request=GetCapabilities"
+	}).
+	    success(function(data, status, headers, config) {
+		// console.log (data);
+		if (data != null) {
+		    var response = x2js.xml_str2json (data)
+		    // console.log (JSON.stringify (response));
+
+		    var processes = response.Capabilities.ProcessOfferings.Process;
+		    var indicators = [];
+
+		    for (var i = 0; i < processes.length; i++) {
+			indicators.push (processes[i].Identifier.__text);
+		    }
+		    $scope.indicators = indicators;
+		    $scope.indicator = $scope.indicators.length > 0 ? $scope.indicators[0] : null;
+		} else {
+		    alert ("Got no Capabilities from WPS!");
+		}
+	    }).
+	    error(function(data, status, headers, config) {
+		alert ("WPS server " + $scope.wpsEndpoint + " not available");
+	    });
+    };
+
+    $scope.loadIndicators();
+
 });
--- wps/processes/__init__.py
+++ wps/processes/__init__.py
@@ -1,1 +1,1 @@
-__all__=['lifeIndicator', 'deathsIndicator', 'seriouslyDeterioratedIndicator', 'improvedIndicator']
+__all__=['deathsIndicator', 'seriouslyDeterioratedIndicator', 'improvedIndicator', 'lifeIndicator']
