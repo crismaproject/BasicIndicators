1370786cbbb51d2eedc2cc28a3ad8f2c63bb2ef6
Peter Kutschera <peter.kutschera@ait.ac.at>
2014-04-23 15:50:53 +0200

    updated OrionListener for new data format - still untested

--- web/OrionListener.py
+++ web/OrionListener.py
@@ -26,113 +26,149 @@
 """
 
 import os, sys
-from lxml import etree
+import json
+import io
 import requests
 import string
 import time
-from sys import stderr
+
+# list of indicators to calculate for each new worldstate
+indicators = [
+    'lifeIndicator', 
+    'deathsIndicator',
+    'seriouslyDeterioratedIndicator',
+    'improvedIndicator'
+    ]
+
+# WPS service
+wps = "http://crisma.ait.ac.at/indicators/pywps.cgi?service=WPS&request=Execute&version=1.0.0&identifier={}&datainputs=ICMMworldstateURL={}"
+
+# The PubSub service-endpoint
+orion="http://crisma.ait.ac.at/orion"
+
+# The listener (this script!) endpoint
+listener="http://crisma.ait.ac.at/indicators/OrionListener.py"
+
+# the last registration id
+subscriptionIdFile = "/home/crisma/public_html/indicators/subscription.json"
+
+# if invoked as command: register as PubSub listener
+if ((len (sys.argv) > 1) and ((sys.argv[1] == "--unsubscribe") or (sys.argv[1] == "--subscribe"))):
+    # is there a subscription id from the last subscription stored? If so unsubscribe!
+    if (os.path.exists (subscriptionIdFile)):
+        # unsubscribe
+        with io.open(subscriptionIdFile) as f:
+            subscription = json.load(f)
+            print "unsubscribe: "
+            if ('subscribeResponse' in subscription):
+                data = {
+                    'subscriptionId' : subscription['subscribeResponse']['subscriptionId']
+                    }
+                params = {}
+                headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+                response = requests.post("{}/NGSI10/unsubscribeContext".format (orion), data=json.dumps (data), params=params, headers=headers) 
+                # unsubscription = response.json() if callable (response.json) else response.json
+                print response.text
+        os.remove (subscriptionIdFile)
+    if (sys.argv[1] == "--subscribe"):
+        print "subscribe: "
+        # condValues was ["time"], but that is not ceccassary.
+        data = {
+            "entities": [
+                {
+                    "type": "CRISMA.worldstates",
+                    "isPattern": "true",
+                    "id": ".*"
+                    }
+                ],
+            "reference": listener,
+            "duration": "PT5M",
+            "notifyConditions": [
+                {
+                    "type": "ONCHANGE",
+                    "condValues": [
+                        "dataslot_OOI-worldstate-ref"
+                        ]
+                    }
+                ]
+            }
+        params = {}
+        headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+        response = requests.post("{}/NGSI10/subscribeContext".format (orion), data=json.dumps (data), params=params, headers=headers) 
+        # subscription = response.json() if callable (response.json) else response.json
+        with io.open (subscriptionIdFile, "w") as f:
+            f.write (response.text)
+        print response.text
+    exit (0)
+
+
+# code to be executed if invoced as CGI script
 
 print "Content-Type: text/plain"    # HTML is following
 print                               # blank line, end of headers
 print "<!-- The OrionBroker Listener says: Thanks for your message -->"
 
+
 # os.getenv("QUERY_STRING")
 
 data = sys.stdin.read()
 
-#print >>stderr, "\n\n"
-#print >>stderr, data 
-#print >>stderr, "\n\n"
+print >>sys.stderr, "\n\n"
+print >>sys.stderr, data 
+print >>sys.stderr, "\n\n"
 
 
-root = etree.fromstring (data)
+# since subscription was done using json the notification is also formatted as json
 
-# root.tag might be:
-# <notifyContextAvailabilityRequest> : New WorldState
-# <notifyContextRequest>: OOI changed
-print >>stderr, "OrionListener got a {}".format (root.tag)
+notification = json.loads (data)
 
-# Example:
-#
-# <notifyContextRequest>
-#   <subscriptionId>52efb40c0000000cf18e0248</subscriptionId>
-#   <originator>localhost</originator>
-#   <contextResponseList>
-#     <contextElementResponse>
-#       <contextElement>
-#         <entityId type="WorldState" isPattern="false">
-#           <id>75</id>
-#         </entityId>
-#         <contextAttributeList>
-#           <contextAttribute>
-#             <name>creation</name>
-#             <type>creation</type>
-#             <contextValue>1391441007</contextValue>
-#           </contextAttribute>
-#         </contextAttributeList>
-#       </contextElement>
-#       <statusCode>
-#         <code>200</code>
-#         <reasonPhrase>OK</reasonPhrase>
-#       </statusCode>
-#     </contextElementResponse>
-#   </contextResponseList>
-# </notifyContextRequest>
+elems = notification["contextResponses"]
+
+for elem in elems:
+    for attr in elem["contextElement"]["attributes"]:
+        if attr["name"].startswith ("worldstate"):
+            wsURL = attr["value"]["URI"]
+            for indicator in indicators:
+                print >>stderr, "Start indicator {} for {}".format (indicator, wsURL)
+                response = requests.get(wps.format (indicator, wsURL))
+                print >>stderr, response.text
+            
+
+exit (0)
 
 
-indicators = [
-    'lifeIndicator', 
-    'deathsIndicator',
-    'seriouslyDeterioratedIndicator'
-    ]
-wps = "http://crisma.ait.ac.at/indicators/pywps.cgi?service=WPS&request=Execute&version=1.0.0&identifier={}&datainputs=WorldStateId={}"
-orion = "http://crisma.ait.ac.at/orion/NGSI10/updateContext"
-
-if "notifyContextRequest" == root.tag:
-    # We are only registered for "creation" updates, so no need to check for this
-    # get id of new WorldState
-    idElem = root.find ("./contextResponseList/contextElementResponse/contextElement/entityId/id")
-    wsid = idElem.text
-
-    # trigger indicator calculations
-    for indicator in indicators:
-        response = requests.get(wps.format (indicator, wsid))
-        print >>stderr, response.text
-        xmlheaderlen = len ('<?xml version="1.0" encoding="utf-8"?> ')
-        result = etree.fromstring (response.text[xmlheaderlen:])
-        successElem = result.find (".//{http://www.opengis.net/wps/1.0.0}ProcessSucceeded")
-        if successElem is not None:
-            identifierElems = result.findall (".//{http://www.opengis.net/wps/1.0.0}Output/{http://www.opengis.net/ows/1.1}Identifier")
-            for elem in identifierElems:
-                if elem.text == 'indicator':
-                    dataElem = elem.find ("../{http://www.opengis.net/wps/1.0.0}Data/{http://www.opengis.net/wps/1.0.0}LiteralData")
-                    uri = dataElem.text
-                    # print >>stderr, uri
-                    psRequest = """<?xml version="1.0" encoding="UTF-8"?>
-<updateContextRequest>
-  <contextElementList>
-    <contextElement>
-      <entityId type="WorldState" isPattern="false">
-        <id>{}</id>
-      </entityId>
-      <contextAttributeList>
-        <contextAttribute>
-          <name>{}</name>
-          <type>indicator</type>
-          <contextValue>{} {}</contextValue>
-        </contextAttribute>
-      </contextAttributeList>
-    </contextElement>
-  </contextElementList>
-  <updateAction>APPEND</updateAction>
-</updateContextRequest>""".format (wsid, indicator, time.time(), uri)
-
-                    #print >>stderr, psRequest
-        
-                    # (curl $endpoint/NGSI10/updateContext -s -S --header 'Content-Type: application/xml' -d @- | xmllint --format - ) <<EOF
-
-                    answer = requests.post (orion, data=psRequest, headers={'Content-Type' : 'application/xml'})
-
-                    #print >>stderr, answer.text
-
+exampleNotification = {
+   "subscriptionId" : "5357b2b3000000dea5adf59d",
+   "originator" : "localhost",
+   "contextResponses" : [
+     {
+       "contextElement" : {
+         "type" : "CRISMA.worldstates",
+         "isPattern" : "false",
+         "id" : "1_2",
+         "attributes" : [
+           {
+             "name" : "time",
+             "type" : "",
+             "value" : "1398256494"
+           },
+           {
+             "name" : "worldstate_Baseline",
+             "type" : "Test baseline",
+             "value" : "{\\"operation\\":\\"updated\\",\\"time\\":1398256494,\\"URI\\":\\"http://crisma.cismet.de/pilotC/icmm_api/CRISMA.worldstates/2\\"}"
+           },
+           {
+             "name" : "dataslot_OOI-worldstate-ref",
+             "type" : "Test baseline",
+             "value" : "{\\"operation\\":\\"created\\",\\"time\\":1398256492,\\"URI\\":\\"http://crisma.cismet.de/pilotC/icmm_api/CRISMA.dataitems/3\\"}"
+           }
+         ]
+       },
+       "statusCode" : {
+         "code" : "200",
+         "reasonPhrase" : "OK"
+       }
+     }
+   ]
+ }
 
